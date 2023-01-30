777f6c38e1c8ad493ba4761552f2abdd651fcda1
dripbert <robababatzen@protonmail.ch>
2023-01-30 21:15:50 +0100

    display notes next to calendar

--- nican_tui.nim
+++ nican_tui.nim
@@ -23,6 +23,10 @@ hide_cursor()
 var tb = new_terminal_buffer(terminal_width(), terminal_height())
 # tb.draw_rect(0, 0,  40, 5)
 
+proc get_notes(f: string): seq =
+  let notes = read_file(f).split('\n')
+  return notes
+
 proc draw_days(): void =
   tb.write(0, 0, reset_style, bg_black, fg_white, cmy)
   var day: string = ""
@@ -45,14 +49,42 @@ proc draw_cal(c: int): void =
       y += 1
       x = 0
 
-proc draw_day_info(d: int): void =
-  tb.write(50, 1, reset_style, bg_black, fg_white, $cmmm & " " & $d & " " &  $cy)
+const msg_width = 43
+proc norm_len(str: string): string =
+  if len(str) > msg_width:
+    return str[0..^(msg_width - 3)] & "..."
 
+  return str & repeat(" ", (msg_width - len(str)) )
+
+proc pr_col(pr: int): string =
+  if pr == 0: return "\e[31m"
+  elif pr == 1: return "\e[33m"
+  return "\e[32m"
 
 var width:  int = terminal_width()
 var height: int = terminal_height()
 
+proc draw_notes(x: int, y: int, notes: seq[string], day: int): void =
+  var line: string
+  var snote: seq[string]
+  for note in notes:
+    if note == "": continue
+    snote = split(note, "|")
+    if snote[2] != $cym & "-" & $day: continue
+    # line = pr_col(parse_int(snote[0])) & snote[0] & "\e[0m  | [ \"\e[1m" & norm_len(snote[1]) & "\e[0m\" ] | \e[34m" & snote[2]
+    line = snote[0] & "  | [ \"" & norm_len(snote[1]) & "\" ] | " & snote[2]
+    tb.write(x, y, reset_style, bg_black, fg_white, line)
+
+const notes_file = "/home/robert/.nimem_notes"
+let notes: seq[string] = get_notes(notes_file)
+
+proc draw_day_info(d: int): void =
+  tb.write(40, 1, reset_style, bg_black, fg_white, $cmmm & " " & $d & " " &  $cy)
+  draw_notes(40, 2, notes, d)
+
+
 var curs:   int = parse_int(cdd)
+
 
 while true:
   var key = get_key()
